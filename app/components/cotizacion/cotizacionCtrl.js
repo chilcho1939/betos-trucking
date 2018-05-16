cf8d634112df910f890755108c1dab637ac54397
Gildardo Ortiz Mercado <gildardo.ortiz@seguritech.com>
2018-05-16 18:10:28 -0500

    se agrega seccion de cotizacion

--- app/components/cotizacion/cotizacionCtrl.js
+++ app/components/cotizacion/cotizacionCtrl.js
@@ -1,3 +1,13 @@
 myApp.controller('cotizacionCtrl', ['$scope', function($scope) {
 
+    $scope.cotizacionObject = {};
+
+    $scope.calcularCotizacion = function(isValid) {
+        if(isValid) {
+            alert("formulario valido")
+            console.log($scope.cotizacionObject)
+        } else {
+            alert("formulario invalido")
+        }
+    }
 }]);
