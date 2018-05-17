56cc225bd4f1afb75c52c9c56b5c3862d925dd60
Gildardo Ortiz Mercado <gildardo.ortiz@seguritech.com>
2018-05-17 10:40:04 -0500

    se finaliza la validacion de formulario de cotizaciones

--- app/components/cotizacion/cotizacionCtrl.js
+++ app/components/cotizacion/cotizacionCtrl.js
@@ -2,13 +2,6 @@ myApp.controller('cotizacionCtrl', ['$scope', function($scope) {
 
     $scope.cotizacionObject = {};
 
-    $scope.calcularCotizacion = function(isValid) {
-        if(isValid) {
-            alert("formulario valido")
-            console.log($scope.cotizacionObject)
-        } else {
-            alert("formulario invalido")
-            console.log($scope.cotizacionObject)
-        }
+    $scope.calcularCotizacion = function() {
     }
 }]);
