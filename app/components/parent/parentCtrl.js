98cba64de3aac49337934095f5355599a2501ade
Gildardo Ortiz Mercado <gildardo.ortiz@seguritech.com>
2018-05-25 18:42:39 -0500

    Se agregan estados

--- app/components/parent/parentCtrl.js
+++ app/components/parent/parentCtrl.js
@@ -1,8 +1,4 @@
 myApp.controller('parentCtrl', ['$scope', '$route', '$rootScope','$transitions', function ($scope, $route, $rootScope, $transitions) {
-    // $scope.$on('$stateChangeSuccess', function(newVal, oldVal) {
-    //     $window.scollTo(0,0)
-    // });
-    // $rootScope.$on('$stateChangeStart', function() { $window.scollTo(0,0) }); 
     $transitions.onSuccess({}, function(trans) {
         document.body.scrollTop = document.documentElement.scrollTop = 0;
     });
