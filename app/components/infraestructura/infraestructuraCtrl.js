06f68ddefb9ebf50af7a4de0f33b2c1c6d65e07c
Gildardo Ortiz Mercado <gildardo.ortiz@seguritech.com>
2018-08-02 17:41:03 -0500

    cambios finales

--- app/components/infraestructura/infraestructuraCtrl.js
+++ app/components/infraestructura/infraestructuraCtrl.js
@@ -1,5 +1,3 @@
 myApp.controller('infraestructuraCtrl', ['$scope', function($scope) {
-    $scope.abrirSitio = function(sitio) {
-        window.open(sitio, '_blank');
-    }
+    
 }]);
--- app/components/parent/parentCtrl.js
+++ app/components/parent/parentCtrl.js
@@ -2,4 +2,8 @@ myApp.controller('parentCtrl', ['$scope', '$route', '$rootScope','$transitions',
     $transitions.onSuccess({}, function(trans) {
         document.body.scrollTop = document.documentElement.scrollTop = 0;
     });
+
+    $scope.abrirSitio = function(sitio) {
+        window.open(sitio, '_blank');
+    }
 }]);
